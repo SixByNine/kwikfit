147c4b6ab8bcdd57e9e29c1625c826c426298b3d
Michael Keith <mkeith@pulsarastronomy.net>
2014-05-27 20:29:09 +0100

    Initial commit

---
+++ src/kwikfit.c
@@ -0,0 +1,11 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+//  Copyright (C) 2014 Michael J. Keith, University of Manchester
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+#include <TKfit.h>
+
+void kwikfit();
